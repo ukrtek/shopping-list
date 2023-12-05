c1112f1a3478a121530e0c1d46a8e7ad12eb651a
Maria-Yemelianova <yemelianova.me@gmail.com>
2023-12-05 22:17:28 +0100

    implement adding and removing items to the list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import ItemAdd from './components/ItemAdd.js';
+import ItemList from './components/ItemList.js';
 
 function App() {
 	const [items, setItems] = useState([]);
@@ -8,11 +9,17 @@ function App() {
 		setItems([...items, item]);
 	};
 
+  const removeItem = (index) => {
+    // _ here is a convention to indicate that we are ignoring the value
+    // filter() takes two arguments: the current item and the index
+    setItems(items.filter((_, i) => i !== index));
+  };
+
   return (
     <div>
       <h1>Shopping List</h1>
 			<ItemAdd onAddItem={addItem} />
-      {/* Components will be added here in later steps */}
+      <ItemList items={items} onRemoveItem={removeItem} />
     </div>
   );
 }
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,4 +1,19 @@
-function ItemList() {
-	const [items, setItems] = useState([]);
-	
+import React from 'react';
+
+function ItemList({ items, onRemoveItem }) {
+  return (
+    <div className="item-list">
+      <h3>Items</h3>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>
+						{item}
+						<button onClick={() => onRemoveItem(index)}>Remove</button>
+						</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+export default ItemList;
